b80714605740e8a4029d6f4637e7ec97f4d5fab5
Jenes Damasceno Junior <jenes.junior2@gmail.com>
2022-07-09 17:25:02 -0300

    exercicios em R dados do enad

---
+++ R/dadosnutricaoenad.R
@@ -0,0 +1,11 @@
+# CHAMAR AS LIBRARY QUE SE VAI TARBALHAR
+
+
+
+# nutrição
+
+# Codigo do curso = CO_GRUPO == 28
+# Codigo localidade = CO_UF_CURSO == 43
+
+# Codigo participação com resposta válida = TP_PR_GER == 555
+# Codigo nota bruta da prova = NT_GER
